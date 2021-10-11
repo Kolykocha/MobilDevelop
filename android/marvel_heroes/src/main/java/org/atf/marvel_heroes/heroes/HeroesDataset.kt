1610fe9b8b60096fba7c4d4397ef6e314511a4f4
Kirill <kirillbalaghanskiii@gmail.com>
2021-10-11 17:12:04 +0600

    [+] android app. Marvel heroes

---
+++ android/marvel_heroes/src/main/java/org/atf/marvel_heroes/heroes/HeroesDataset.kt
@@ -0,0 +1,16 @@
+package org.atf.marvel_heroes.heroes
+
+import androidx.core.graphics.toColorInt
+import org.atf.marvel_heroes.R
+import org.atf.marvel_heroes.heroes.model.Hero
+
+class HeroesDataset {
+
+    fun getHeroes(): List<Hero> = listOf(
+        Hero(1, "Scarlet Spider", R.drawable.ss, "#39b4bc".toColorInt()),
+        Hero(2, "Gwen Spider", R.drawable.gp, "#5c3a77".toColorInt()),
+        Hero(3, "Magik", R.drawable.magik, "#edc93f".toColorInt()),
+        Hero(4, "Knull", R.drawable.knull, "#65d6da".toColorInt())
+)
+
+}
