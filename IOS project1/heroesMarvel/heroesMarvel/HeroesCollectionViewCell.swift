6eda53d4b3e8756693d95246ba4ffcefc0a3eaf8
Kirill <kirillbalaghanskiii@gmail.com>
2021-10-25 14:34:53 +0600

    IOS1

---
+++ IOS project1/heroesMarvel/heroesMarvel/HeroesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeroesCollectionViewCell.swift
+//  heroesMarvel
+//
+//  Created by Кирилл Балаганский on 25.10.2021.
+//
+
+import UIKit
+
+class HeroesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var heroesImageView: UIImageView!
+    
+    @IBOutlet weak var heroesLabelView: UILabel!
+}
