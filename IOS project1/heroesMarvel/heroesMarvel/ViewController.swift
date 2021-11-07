7afa236b817259a15e94fe7db790634c215c1ec0
Kirill <kirillbalaghanskiii@gmail.com>
2021-11-07 18:27:38 +0600

    fix№2

--- IOS project1/heroesMarvel/heroesMarvel/ViewController.swift
+++ IOS project1/heroesMarvel/heroesMarvel/ViewController.swift
@@ -33,7 +33,36 @@ class ViewController: UIViewController {
         }
         view.backgroundColor = .black
         collectionView.backgroundColor = .black
+		startTimer()
     }
+
+    func startTimer() {
+
+        let timer =  Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.scrollAutomatically), userInfo: nil, repeats: true)
+    }
+
+
+    @objc func scrollAutomatically(_ timer1: Timer) {
+
+        if let coll  = collectionView {
+            for cell in coll.visibleCells {
+                let indexPath: IndexPath? = coll.indexPath(for: cell)
+                if ((indexPath?.row)!  <  5 - 1){
+                    let indexPath1: IndexPath?
+                    indexPath1 = IndexPath.init(row: (indexPath?.row)! + 1, section: (indexPath?.section)!)
+
+                    coll.scrollToItem(at: indexPath1!, at: .right, animated: true)
+                }
+                else{
+                    let indexPath1: IndexPath?
+                    indexPath1 = IndexPath.init(row: 0, section: (indexPath?.section)!)
+                    coll.scrollToItem(at: indexPath1!, at: .left, animated: true)
+                }
+
+            }
+        }
+    }
+}
 }
 
 
